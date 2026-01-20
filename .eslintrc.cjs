34ea78be354189c8fdaace407e356c794cea16d6
Arturo Guerra <arturoguerrab12@gmail.com>
2026-01-20 16:55:50 -0300

    vercel fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
 }
